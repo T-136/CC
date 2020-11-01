166f83f2e9eb7491a8dec2f5243f9ecee339eed6
Noah Kamara <40308075+noahkamara@users.noreply.github.com>
2020-11-01 18:39:59 +0100

    Added download script

---
+++ Utility/downloader.lua
@@ -0,0 +1,15 @@
+-- GET INPUT
+print(" filpath: ")
+local filpath = read()
+print("github path: ")
+local url = "https://raw.githubusercontent.com/" + read()
+
+-- URL REQUEST
+local response = http.get(url)
+local content = response.readAll()
+response.close()
+
+-- WRITE TO FILE
+local file = fs.open(filpath, "w")
+file.write(content)
+file.close()
